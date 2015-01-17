4782457384d4377f474cf82c06ee023e6b22e240
Jeremy Wilken <gnomation@gnomeontherun.com>
2015-01-17 17:01:08 -0600

    change default config example

---
+++ config/www/js/app.js
@@ -0,0 +1,16 @@
+angular.module('App', ['ionic'])
+
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.style('striped').position('bottom');
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
